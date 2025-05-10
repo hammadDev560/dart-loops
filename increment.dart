2f00500d81fface224e376df1c435bd24887e368
hammadDev560 <hammadvcr@gmail.com>
2025-05-11 00:46:14 +0500

    Add files via upload

---
+++ increment.dart
@@ -0,0 +1,31 @@
+void main() {
+  // for (int a = 1; a <= 10; a++) {
+  //   print(a * 2);
+  // }
+
+  // int num = 5;
+  // for (int a = 1; a <= 10; a++) {
+  //   print("$num X $a = ${num * a}");
+  // }
+
+  // for (var i = 2; i <= 2; i++) {
+  //   for (var j = 1; j <= 10; j++) {
+  //     print("$i X $j = ${i * j}");
+  //   }
+  // }
+
+  // int i = 10;
+  // while (i >= 1) {
+  //   print(i);
+  //   i--;
+  // }
+
+  // int num = 25;
+  // if (num > 0) {
+  //   print("positive");
+  // } else if (num < 0) {
+  //   print("negative");
+  // } else {
+  //   print("number is zero");
+  // }
+}
